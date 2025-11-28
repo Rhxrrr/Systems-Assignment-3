bc1f7985284ded54a1da1ab0c917244fd0ecfac6
fardinalam1234 <90734889+fardinalam1234@users.noreply.github.com>
2025-11-27 19:36:52 -0500

    Implement basic TCP client in client.c

---
+++ q2/client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 9001
+#define BUFFER 1024
+
+int main() {
+    int sock;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr); // connect
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection failed\n");
+        return 1;
+    }
+
+    while (1) {
+        printf("\nEnter command (TIME, DATE, EXIT): "); // read user input command
+        fgets(buffer, BUFFER, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        send(sock, buffer, strlen(buffer), 0); // send to server
+
+        if (strcmp(buffer, "EXIT") == 0) {
+            int bytes = recv(sock, buffer, BUFFER - 1, 0);
+            buffer[bytes] = '\0';
+            printf("Server: %s\n", buffer);
+            break;
+        }
+
+        int bytes = recv(sock, buffer, BUFFER - 1, 0); // recieve response from server
+        buffer[bytes] = '\0';
+        printf("Server: %s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
