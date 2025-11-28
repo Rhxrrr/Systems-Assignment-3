923033d04180dfc80b17ef0f2bfff2bb2a7963d4
fardinalam1234 <90734889+fardinalam1234@users.noreply.github.com>
2025-11-27 19:47:12 -0500

    Update client.c
    Added team information and course details as comments.

--- q2/client.c
+++ q2/client.c
@@ -1,3 +1,9 @@
+/*
+ * Team Name: Hamzah Alhamadani, Fardin Alam, Mohammed Abdulaziz
+ * Group Number: 24
+ * Course: CSCI 3310U
+ * Assignment: 3
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -47,3 +53,4 @@ int main() {
     close(sock);
     return 0;
 }
+
